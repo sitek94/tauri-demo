be5f3fb897f0bff858196423d1573184b801411c
Maciek Sitkowski <msitkowski94@gmail.com>
2025-01-29 16:06:38 +0100

    notify about new app version

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-# Tauri + Vanilla
+# Tauri Demo
 
-This template should help get you started developing with Tauri in vanilla HTML, CSS and Javascript.
+A minimal Tauri v2 demo with automatic release after merging to main branch and automatic app update check.
+
+## Install on macOS
+
+1. Download the app from the [releases page](https://github.com/sitek94/tauri-demo/releases)
+2. Open the app
+3. If you get a warning saying that "the app is damaged":
+
+   ```
+   xattr -d com.apple.quarantine /Applications/tauri-demo.app
+   ```
 
 ## Recommended IDE Setup
 
-- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+- [VS Code](https://code.visualstudio.com/) +
+  [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) +
+  [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
--- src/main.js
+++ src/main.js
@@ -1,24 +1,9 @@
-const {invoke} = window.__TAURI__.core
+const {getVersion} = window.__TAURI__.app
 const {check} = window.__TAURI_PLUGIN_UPDATER__
 const {relaunch} = window.__TAURI_PLUGIN_PROCESS__
 
-let greetInputEl
-let greetMsgEl
-
-async function greet() {
-  greetMsgEl.textContent = await core.invoke('greet', {
-    name: greetInputEl.value,
-  })
-}
-
 window.addEventListener('DOMContentLoaded', () => {
-  greetInputEl = document.querySelector('#greet-input')
-  greetMsgEl = document.querySelector('#greet-msg')
-  document.querySelector('#greet-form').addEventListener('submit', e => {
-    e.preventDefault()
-    greet()
-  })
-
+  updateAppTitle()
   checkForUpdates()
 })
 
@@ -28,26 +13,54 @@ async function checkForUpdates() {
     console.log(
       `found update ${update.version} from ${update.date} with notes ${update.body}`,
     )
-    let downloaded = 0
-    let contentLength = 0
 
-    await update.downloadAndInstall(event => {
-      switch (event.event) {
-        case 'Started':
-          contentLength = event.data.contentLength
-          console.log(`started downloading ${event.data.contentLength} bytes`)
-          break
-        case 'Progress':
-          downloaded += event.data.chunkLength
-          console.log(`downloaded ${downloaded} from ${contentLength}`)
-          break
-        case 'Finished':
-          console.log('download finished')
-          break
-      }
-    })
-
-    console.log('update installed')
-    await relaunch()
+    const alert = createUpdateAlert(update.version, updateApp)
+    document.body.prepend(alert)
   }
+}
+
+async function updateApp() {
+  let downloaded = 0
+  let contentLength = 0
+
+  await update.downloadAndInstall(event => {
+    switch (event.event) {
+      case 'Started':
+        contentLength = event.data.contentLength
+        console.log(`started downloading ${event.data.contentLength} bytes`)
+        break
+      case 'Progress':
+        downloaded += event.data.chunkLength
+        console.log(`downloaded ${downloaded} from ${contentLength}`)
+        break
+      case 'Finished':
+        console.log('download finished')
+        break
+    }
+  })
+
+  console.log('update installed')
+  await relaunch()
+}
+
+function createUpdateAlert(version, onUpdateClick) {
+  const alert = document.createElement('div')
+  alert.classList.add('alert')
+
+  const p = document.createElement('p')
+  p.textContent = `New version available: ${version}`
+  alert.appendChild(p)
+
+  const button = document.createElement('button')
+  button.textContent = 'Update'
+  button.addEventListener('click', onUpdateClick)
+  alert.appendChild(button)
+
+  return alert
+}
+
+async function updateAppTitle() {
+  const version = await getVersion()
+  const versionEl = document.querySelector('#version')
+  versionEl.textContent = version
 }
